8c90312691bce6a9be10267bd00946f01fa889b0
Paige Godfrey <paagodfrey@gmail.com>
2020-03-05 12:56:24 -0800

    fixed search bar styling

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -25,20 +25,23 @@ function SearchBar({ filterBySearchObject }) {
 
   return (
     <div className="SearchBar mb-4">
-      <form onSubmit={handleSubmit} className="SearchBar-form form-inline">
-          <input className="form-control form-control-lg flex-grow-1"
-            id="search"
+      <form className="form-inline" onSubmit={handleSubmit}>
+        <div className="input-group flex-grow-1">
+          <input className="form-control form-control-lg"
             name="search"
             type="text"
             placeholder="Search.."
             value={searchObject.search}
             onChange={handleChange}
           />
-          <div className="input-group-append">
-            <button type="submit" className="input-group-text btn btn-lrg btn-primary">Submit</button>
+          <div className="input-group-btn">
+            <button className="btn btn-lg btn-primary" type="submit">
+              Submit
+            </button>
           </div>
+        </div>
       </form>
     </div>
-      )
-    }
+  )
+}
 export default SearchBar;
