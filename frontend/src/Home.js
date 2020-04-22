eaae17790ee423590c76d83c98ecfd8d1c714280
sairina <sairina.merinotsui@gmail.com>
2020-04-21 23:52:01 -0700

    updated styling for home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,35 +1,25 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "./UserContext";
-import headerImg from "./you-x-ventures-X8H8vPcelPk-unsplash.jpg";
+import './Home.css';
 
 const Home = () => {
   const { currentUser } = useContext(UserContext);
   return (
-    <div className="Home d-flex" style={{ height: '50vh' }}>
+
+    <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <div className="row">
-          <div className="col-md-7">
+        <h1 className="display-3 text-right">Jobly</h1>
+        <p className="lead text-right">All the jobs in one, convenient place.</p>
+        {currentUser ? (
+          <h2>Welcome back{currentUser.first_name ? `, ${currentUser.first_name}!` : '!'}</h2>
+        ) : (
             <div>
-              <img src={headerImg} className="img-fluid" alt="Jobly" />
-            </div>
-          </div>
-          <div className="col-md-5 ml-auto d-flex align-items-center mt-4 mt-md-0">
-            <div>
-              <h1>Jobly</h1>
-              <p className="lead">All the jobs in one, convenient place.</p>
-              {currentUser ? (
-                <h2>Welcome back{currentUser.first_name ? `, ${currentUser.first_name}!` : '!'}</h2>
-              ) : (
-                <div>
-                  <Link className="btn btn-primary font-weight-bold" to="/register">
-                    Get Started
+              <Link className="btn btn-info font-weight-bold float-right" to="/register">
+                Get Started
                   </Link>
-                </div>
-              )}
             </div>
-          </div>
-        </div>
+          )}
       </div>
     </div>
   );
